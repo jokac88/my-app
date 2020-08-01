c578c5331ce2fe4ce10b979e2a940119654bf892
Ivan Jovkovic <ivan.jovkovic88@gmail.com>
2020-08-01 16:19:28 +0200

    upload project

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ personalInfo.firstName }}</h1>
+    <h3>{{ personalInfo.lastName }}</h3>
+    <h6>{{ personalInfo.age }}</h6>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  head() {
+    return {
+      title: "Ivan Jovkovic | Front-End Developer",
+    };
+  },
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch("personalInfo/fetchPersonalInfo");
+    } catch (e) {
+      error({
+        message: "Error",
+      });
+    }
+  },
+  computed: mapState({
+    personalInfo: (state) => state.personalInfo.personalInfo,
+  }),
+};
+</script>
