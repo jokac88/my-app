5c30837a02719c3d3e5b70b4645377dd6f98839a
Ivan Jovkovic <ivan.jovkovic88@gmail.com>
2020-08-06 15:24:15 +0200

    add

--- services/axios.js
+++ services/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `https://my-json-server.typicode.com/jokac88/my-app`,
+  // baseURL: `https://my-json-server.typicode.com/jokac88/my-app`,
+  baseURL: `http://localhost:3000`,
   withCredentials: false,
   headers: {
     Accept: "application/json",
