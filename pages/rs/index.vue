25f9f25fa7b4e954cae229b16398d9c1b3af1a2c
Ivan Jovkovic <ivan.jovkovic88@gmail.com>
2020-08-24 12:25:10 +0200

    fix

---
+++ pages/rs/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <Stranica :data="data" />
+</template>
+
+<script>
+import Stranica from "@/components/rs/Stranica.vue";
+import { mapState } from "vuex";
+
+export default {
+  components: {
+    Stranica,
+  },
+  async fetch({ store, params, error }) {
+    try {
+      await store.dispatch("store/fetchData", "/b/5f41a7a3514ec5112d0c399b");
+      // await store.dispatch("store/fetchData", params.lang || "/rs");
+    } catch (e) {
+      error({
+        message: "Greška",
+      });
+    }
+  },
+  async mounted() {
+    await this.localStorage();
+  },
+  methods: {
+    localStorage() {
+      localStorage.setItem("data", JSON.stringify(this.data));
+    },
+  },
+  computed: mapState({
+    data: (state) => state.store.data.rs,
+  }),
+};
+</script>
