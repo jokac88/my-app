f84cfe0cb2d05696f10ef5bc3b241b1f6d2db03d
Ivan Jovkovic <ivan.jovkovic88@gmail.com>
2020-08-30 13:43:34 +0200

    fix

--- store/store.js
+++ store/store.js
@@ -29,7 +29,7 @@ export const actions = {
   loading({ commit }, loading) {
     setTimeout(() => {
       commit("SET_LOADING", loading);
-    }, 2500);
+    }, 2000);
   },
   toggleDarkMode({ commit }) {
     commit("SET_DARKMODE");
